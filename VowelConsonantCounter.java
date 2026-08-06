0af48bcd3d45221098f9610958335e3ca2f4e65a
Sakhi Mali <63789114+sakhi-shraddha-sst@users.noreply.github.com>
2026-08-06 19:04:52 +0530

    feat: add VowelConsonantCounter.java program

---
+++ VowelConsonantCounter.java
@@ -0,0 +1,22 @@
+public class VowelConsonantCounter {
+    public static void main(String[] args) {
+        String str = "Automation World";
+        str = str.toLowerCase();
+
+        int vowels = 0, consonants = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels + ", Consonants: " + consonants);
+    }
+}
