e3b064eb39aaa93cfe24a488bcd848f458654513
Sakhi Mali <63789114+sakhi-shraddha-sst@users.noreply.github.com>
2026-08-07 15:48:07 +0530

    feat: add CharOccurrence.java program

---
+++ CharOccurrence.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class CharOccurrence {
+    public static void main(String[] args) {
+        String str = "automation";
+
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+        for (char ch : str.toCharArray()) {
+            if (charCount.containsKey(ch)) {
+                charCount.put(ch, charCount.get(ch) + 1);
+            } else {
+                charCount.put(ch, 1);
+            }
+        }
+
+        System.out.println(charCount);
+    }
+}
