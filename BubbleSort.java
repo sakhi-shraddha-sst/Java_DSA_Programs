3295b08adf38320a6e521ff67ef5dbf5d784170a
Sakhi Mali <63789114+sakhi-shraddha-sst@users.noreply.github.com>
2026-09-01 20:40:57 +0530

    feat: add BubbleSort.java program

---
+++ BubbleSort.java
@@ -0,0 +1,40 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            // If no swapping occurred in this pass, array is sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Original array: " + java.util.Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println("Sorted array: " + java.util.Arrays.toString(arr));
+
+        // Output:
+        // Original array: [64, 34, 25, 12, 22, 11, 90]
+        // Sorted array: [11, 12, 22, 25, 34, 64, 90]
+    }
+}
