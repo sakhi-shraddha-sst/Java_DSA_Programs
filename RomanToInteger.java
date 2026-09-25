5f2a36e9c376e2e1655e0ab600105905870e7488
Sakhi Mali <63789114+sakhi-shraddha-sst@users.noreply.github.com>
2026-09-25 16:00:21 +0530

    feat: add RomanToInteger.java program

---
+++ RomanToInteger.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        Map<Character, Integer> map = Map.of(
+            'I', 1, 'V', 5, 'X', 10, 'L', 50,
+            'C', 100, 'D', 500, 'M', 1000
+        );
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int value = map.get(s.charAt(i));
+            if (i + 1 < s.length() && value < map.get(s.charAt(i + 1)))
+                result -= value;
+            else
+                result += value;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt(""MCMXCIV"")); // 1994
+    }
+}
